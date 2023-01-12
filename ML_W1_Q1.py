0f0dc092b2af5af75677bcd2a0b923aaa2932f00
shivanagendra.k <shivaaman751@gmail.com>
2023-01-11 20:00:51 -0500

    Machine Learning Week 1 Assignment Question 1

---
+++ ML_W1_Q1.py
@@ -0,0 +1,8 @@
+user = int(input("Enter the First Number: "))
+user1 = int(input("Enter the Second Number: "))
+sum = user+user1
+difference = user-user1
+multiplication = user*user1
+print("The Sum of two numbers is:",sum)
+print("The Difference of two numbers is:",difference)
+print("The Multiplication of two Numbers is: ",multiplication)
