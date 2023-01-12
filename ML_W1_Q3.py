4113f079e312596888223aa3269ae347be6e300f
shivanagendra.k <shivaaman751@gmail.com>
2023-01-11 20:18:05 -0500

    Machine Learning Week 1 Question 3

---
+++ ML_W1_Q3.py
@@ -0,0 +1,7 @@
+#Name = Shiva Nagendra Babu
+user = int(input("Enter the Amount: "))
+years = int(input("Enter the Number of Years: "))
+annual_intrest = 0.06 # 6/100
+Amount = user*years*annual_intrest
+Money = user+Amount
+print("The Total Money is:",Money)
